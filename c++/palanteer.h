7c332c3af7327bdb6ecad8084435801f97bb1530
dfeneyrou <dfeneyrou@gmail.com>
2022-01-05 23:46:25 +0100

    0.7 is Work In Progress

--- c++/palanteer.h
+++ c++/palanteer.h
@@ -1381,8 +1381,8 @@ public:
 #if USE_PL==1 || PL_EXPORT==1
 
 // Library version
-#define PALANTEER_VERSION "0.6"
-#define PALANTEER_VERSION_NUM 600  // Monotonic number. 100 per version component. Official releases are multiple of 100
+#define PALANTEER_VERSION "0.7.dev1"
+#define PALANTEER_VERSION_NUM 601  // Monotonic number. 100 per version component. Official releases are multiple of 100
 
 // Client-Server protocol version
 #define PALANTEER_CLIENT_PROTOCOL_VERSION 3
