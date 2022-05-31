dfeneyrou/palanteer

commit 76eeae52bbe62f153aad0eb03efd05858a6e6dde

Improved global navigation and popup window visibility (they were not always visible)

server/viewer/vwMain.h
@@ -545,7 +545,7 @@ private:
         ImGuiID newDockId = 0xFFFFFFFF; // 0xFFFFFFFF: use the automatic one
         bool isFlameGraph = false;
         bool isFlameGraphDownward = true;
-        bool isWindowSelected = false;
+        bool isWindowSelected = true;
         bool isNew        = true;
         bool isFirstRun   = true;
         int  cmDataIdx    = -1;
@@ -904,7 +904,7 @@ private:
         bool     isCacheDirty   = true;
         bool     isFirstRun     = true;
         bool     isNew          = true;
-        bool     isWindowSelected = false;
+        bool     isWindowSelected = true;
         // Constant data
         bsVec<HistoData> fullResData;
         double absMinValue;

server/viewer/vwMainCommon.cpp
@@ -851,7 +851,7 @@ vwMain::displayPlotContextualMenu(int threadId, const char* rootText, float head
         if(ImGui::BeginCombo("", pmi.comboSelectionString.toChar(), 0)) {
             // None
             bool isSelected = (pmi.comboSelectionExistingIdx==-1 && pmi.comboSelectionNewIdx==-1);
-            if(ImGui::Selectable("", isSelected)) {
+            if(ImGui::Selectable("-", isSelected)) {
                 if(pmi.comboSelectionNewIdx>=0) _plotMenuNewPlotCount[pmi.comboSelectionNewIdx] -= 1;
                 pmi.comboSelectionExistingIdx = pmi.comboSelectionNewIdx = -1;
                 pmi.comboSelectionRemoval = false;
@@ -1055,7 +1055,7 @@ vwMain::displayHistoContextualMenu(float headerWidth, float comboWidth)
         float cursorX = ImGui::GetCursorPosX();
         if(ImGui::BeginCombo("", pmi.comboHistoSelectionString.toChar(), 0)) {
             // Empty
-            if(ImGui::Selectable("", false)) {
+            if(ImGui::Selectable("-", false)) {
                 pmi.comboHistoSelectionString.clear();
                 pmi.comboHistoSelectionIdx = -1;
             }

server/viewer/vwMainHistogram.cpp
@@ -470,7 +470,7 @@ vwMain::drawHistograms(void)
             if(histogram.newDockId!=0xFFFFFFFF) ImGui::SetNextWindowDockID(histogram.newDockId);
             else selectBestDockLocation(true, false);
         }
-        if(histogram.isWindowSelected) {
+        if(histogram.computationLevel==100 && histogram.isWindowSelected) {
             histogram.isWindowSelected = false;
             ImGui::SetNextWindowFocus();
         }
@@ -510,7 +510,7 @@ vwMain::drawHistogram(int histogramIdx)
     const float winHeight = bsMax(ImGui::GetWindowSize().y, 1.f);
     const float mouseX    = ImGui::GetMousePos().x;
     const float mouseY    = ImGui::GetMousePos().y;
-    const bool   isWindowHovered = ImGui::IsWindowHovered();
+    const bool  isWindowHovered = ImGui::IsWindowHovered();
     const float fontHeight   = ImGui::GetTextLineHeight();
     const float topBarHeight = ImGui::GetTextLineHeightWithSpacing();
     const float uMargin   = 5.f;
@@ -518,7 +518,9 @@ vwMain::drawHistogram(int histogramIdx)
     const float pointSize = 3.f;
 
     // Get keyboard focus on window hovering
-    if(isWindowHovered && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) ImGui::SetWindowFocus();
+    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootWindow) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) {
+        ImGui::SetWindowFocus();
+    }
 
     Histogram& h = _histograms[histogramIdx];
     prepareHistogram(h); // Ensure cache is up to date, even at window creation

server/viewer/vwMainLog.cpp
@@ -269,7 +269,9 @@ vwMain::drawLog(LogView& lv)
     const bool isWindowHovered = ImGui::IsWindowHovered();
 
     // Get keyboard focus on window hovering
-    if(isWindowHovered && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) ImGui::SetWindowFocus();
+    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootWindow) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) {
+        ImGui::SetWindowFocus();
+    }
 
     constexpr int maxMsgSize = 256;
     char tmpStr [maxMsgSize];

server/viewer/vwMainMemory.cpp
@@ -1030,7 +1030,9 @@ vwMain::drawMemoryTimeline(int curMwWindowIdx)
     const float fullHeaderHeight = ctx.fullHeaderHeight;
 
     // Get keyboard focus on window hovering
-    if(ctx.isWindowHovered && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) ImGui::SetWindowFocus();
+    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootWindow) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) {
+        ImGui::SetWindowFocus();
+    }
 
     prepareMemoryTimeline(mw); // Ensure cache is up-to-date, even at window creation
     ctx.computeLayout(mw);

server/viewer/vwMainPlot.cpp
@@ -262,7 +262,9 @@ vwMain::drawPlot(int curPlotWindowIdx)
     const float vMargin = ImGui::GetTextLineHeight();
 
     // Get keyboard focus on window hovering
-    if(isWindowHovered && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) ImGui::SetWindowFocus();
+    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootWindow) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) {
+        ImGui::SetWindowFocus();
+    }
 
     // Prepare if cache is dirty (in case of removed curve for instance)
     preparePlot(pw);

server/viewer/vwMainProfile.cpp
@@ -439,7 +439,7 @@ vwMain::drawProfiles(void)
             a = 1.1f; ImGui::PushStyleColor(ImGuiCol_TitleBgActive,      ImVec4(a*c.x, a*c.y, a*c.z, 1.f));
         }
 
-        if(prof.isWindowSelected) {
+        if(prof.computationLevel==100 && prof.isWindowSelected) {
             prof.isWindowSelected = false;
             ImGui::SetNextWindowFocus();
         }
@@ -752,7 +752,9 @@ vwMain::_drawFlameGraph(bool doDrawDownward, Profile& prof)
     ImU32 colorText2     = vwConst::uGrey;
 
     // Get keyboard focus on window hovering
-    if(isWindowHovered && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) ImGui::SetWindowFocus();
+    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootWindow) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) {
+        ImGui::SetWindowFocus();
+    }
 
     auto getValueString = [this] (const vwMain::Profile& prof, double value) {
         if(prof.kind==TIMINGS) return bsString(getNiceDuration((s64)value));

server/viewer/vwMainText.cpp
@@ -259,7 +259,9 @@ vwMain::drawText(Text& t)
     char valueStr[maxMsgSize];
 
     // Get keyboard focus on window hovering
-    if(isWindowHovered && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) ImGui::SetWindowFocus();
+    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootWindow) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused()) {
+        ImGui::SetWindowFocus();
+    }
 
     // Did the user click on the scrollbar? (detection based on an unexpected position change)
     float curScrollPosX = ImGui::GetScrollX();

commit dce83b62e13cab4fbde816c1b9558e5d407dc37a

fix a regression in 76eeae52bbe62f, preventing some popup to open (again)

server/viewer/vwMain.h
@@ -173,6 +173,7 @@ private:
     void synchronizeNewRange(int syncMode, s64 startTimeNs, s64 timeRangeNs);
     void synchronizeText(int syncMode, int threadId, int level, u32 lIdx, s64 timeNs, u32 idToIgnore=(u32)-1);
     void synchronizeThreadLayout(void);
+    void getKeyboardFocusIfWindowHovering(void);
     void allIsDirty(void);
     void ensureThreadVisibility(int syncMode, int threadId);
     inline s64 getUpdatedRange(int deltaWheel, s64 newRangeNs) {

server/viewer/vwMainCommon.cpp
@@ -653,6 +653,29 @@ vwMain::synchronizeThreadLayout(void) // Invalidate the cache
 }
 
 
+void
+vwMain::getKeyboardFocusIfWindowHovering(void)
+{
+    // The automata delaying the keyboard focus capture by 1 frame is required in order
+    // not to prevent the opening of modal windows
+    static int count = -1;
+
+    // Window is hovered and does not have the keyboard focus?
+    if(!_search.isInputPopupOpen && ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows) &&
+       !ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows)) {
+        if(count<0) count = 1; // Arm the count
+        else --count;          // or decrement it
+    }
+    else count = -1;
+
+    // Take the focus after the second frame only
+    if(count==0) {
+        count = -1;
+        ImGui::SetWindowFocus();
+    }
+}
+
+
 // Contextual menu helpers
 // =======================
 

server/viewer/vwMainHistogram.cpp
@@ -518,9 +518,7 @@ vwMain::drawHistogram(int histogramIdx)
     const float pointSize = 3.f;
 
     // Get keyboard focus on window hovering
-    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows)) {
-        ImGui::SetWindowFocus();
-    }
+    getKeyboardFocusIfWindowHovering();
 
     Histogram& h = _histograms[histogramIdx];
     prepareHistogram(h); // Ensure cache is up to date, even at window creation

server/viewer/vwMainLog.cpp
@@ -269,9 +269,7 @@ vwMain::drawLog(LogView& lv)
     const bool isWindowHovered = ImGui::IsWindowHovered();
 
     // Get keyboard focus on window hovering
-    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows)) {
-        ImGui::SetWindowFocus();
-    }
+    getKeyboardFocusIfWindowHovering();
 
     constexpr int maxMsgSize = 256;
     char tmpStr [maxMsgSize];

server/viewer/vwMainMemory.cpp
@@ -1033,9 +1033,7 @@ vwMain::drawMemoryTimeline(int curMwWindowIdx)
     const float fullHeaderHeight = ctx.fullHeaderHeight;
 
     // Get keyboard focus on window hovering
-    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows)) {
-        ImGui::SetWindowFocus();
-    }
+    getKeyboardFocusIfWindowHovering();
 
     prepareMemoryTimeline(mw); // Ensure cache is up-to-date, even at window creation
     ctx.computeLayout(mw);

server/viewer/vwMainPlot.cpp
@@ -262,9 +262,7 @@ vwMain::drawPlot(int curPlotWindowIdx)
     const float vMargin = ImGui::GetTextLineHeight();
 
     // Get keyboard focus on window hovering
-    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows)) {
-        ImGui::SetWindowFocus();
-    }
+    getKeyboardFocusIfWindowHovering();
 
     // Prepare if cache is dirty (in case of removed curve for instance)
     preparePlot(pw);

server/viewer/vwMainProfile.cpp
@@ -753,9 +753,7 @@ vwMain::_drawFlameGraph(bool doDrawDownward, Profile& prof)
     ImU32 colorText2     = vwConst::uGrey;
 
     // Get keyboard focus on window hovering
-    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows)) {
-        ImGui::SetWindowFocus();
-    }
+    getKeyboardFocusIfWindowHovering();
 
     auto getValueString = [this] (const vwMain::Profile& prof, double value) {
         if(prof.kind==TIMINGS) return bsString(getNiceDuration((s64)value));

server/viewer/vwMainText.cpp
@@ -259,9 +259,7 @@ vwMain::drawText(Text& t)
     char valueStr[maxMsgSize];
 
     // Get keyboard focus on window hovering
-    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows)) {
-        ImGui::SetWindowFocus();
-    }
+    getKeyboardFocusIfWindowHovering();
 
     // Did the user click on the scrollbar? (detection based on an unexpected position change)
     float curScrollPosX = ImGui::GetScrollX();

server/viewer/vwMainTimeline.cpp
@@ -1521,9 +1521,7 @@ vwMain::drawTimeline(int tlWindowIdx)
     ctx.colorGap    = vwConst::uLightGrey;
 
     // Get keyboard focus on window hovering
-    if(ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows) && !_search.isInputPopupOpen && !ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows)) {
-        ImGui::SetWindowFocus();
-    }
+    getKeyboardFocusIfWindowHovering();
 
     float scrollbarY = ImGui::GetScrollY();
     float yThread = ctx.winY-scrollbarY;
@@ -1686,7 +1684,7 @@ vwMain::drawTimeline(int tlWindowIdx)
     }
 
     // Double click: range focus on an item (detected above at drawing time)
-    if(ctx.forceRangeNs!=0.) {
+    if(ctx.forceRangeNs!=0) {
         tl.setView(ctx.forceStartNs, ctx.forceRangeNs);
         ctx.nsToPix = ctx.winWidth/tl.timeRangeNs;
         changedNavigation = true;